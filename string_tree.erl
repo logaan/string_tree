392e910f665fdcc585ddfdd915f70b49e018239f
Colin Campbell-McPherson <colin@logaan.net>
2009-01-04 03:27:33 +0900

    changed string_tree:format to return a string. added unit tests.

--- string_tree.erl
+++ string_tree.erl
@@ -1,25 +1,80 @@
 -module(string_tree).
 -export([format/1]).
+-include_lib("eunit/include/eunit.hrl").
 
-format(StringTree) -> format(StringTree, 0).
+% If we made leafs to be just strings then the same method that prints the
+% string in a non-leaf node could be used to handle leaf nodes.
+
+format(StringTree) -> format(StringTree, 0, "").
 
 % Leaves are in the format {String, []}
 % Regular nodes are in the format {String, [Node1, Leaf1, ... NodeN, LeafN]}
-format({String, []}, Indentation) ->
-  indented_print(String, Indentation);
+format({String, []}, Indentation, Output) ->
+  indented_print(String, Indentation, Output);
 
-format({String, Children}, Indentation) ->
-  indented_print(String, Indentation),
-  format(Children, Indentation + 1);
+format({String, Children}, Indentation, Output) ->
+  NewOutput = indented_print(String, Indentation, Output),
+  format(Children, Indentation + 1, NewOutput);
 
-format([H|T], Indentation) ->
-  format(H, Indentation),
-  format(T, Indentation);
+format([H|T], Indentation, Output) ->
+  HeadOutput = format(H, Indentation, Output),
+  format(T, Indentation, HeadOutput);
 
-format([], Indentation) ->
-  ok.
+format([], Indentation, Output) ->
+  Output.
 
-indented_print(String, Indentation) ->
+indented_print(String, Indentation, Output) ->
   IndentationString = string:chars($ , Indentation * 2),
-  io:format("~s- ~s~n", [IndentationString, String]).
+  string:join([Output, IndentationString, "- ", String, "\n"], "").
 
+%
+% FORMAT TESTS
+%
+
+single_leaf_test() ->
+  SingleLeaf = {"Foo", []},
+  ExpectedOutput = "- Foo\n",
+  ?assert(format(SingleLeaf) == ExpectedOutput).
+
+parent_child_test() ->
+  Bar = {"Bar", []},
+  Foo = {"Foo", [Bar]},
+  ExpectedOutput = "- Foo\n  - Bar\n",
+  ?assert(format(Foo) == ExpectedOutput).
+
+siblings_test() ->
+  Bar = {"Bar", []},
+  Baz = {"Baz", []},
+  Foo = {"Foo", [Bar, Baz]},
+  ExpectedOutput = "- Foo\n  - Bar\n  - Baz\n",
+  ?assert(format(Foo) == ExpectedOutput).
+
+family_tree_test() ->
+  FamilyTree = {
+    "Me",
+    [
+      {
+        "Mother",
+        [
+          { "Grandmother", [] },
+          { "Grandfather", [] }
+        ]
+      },
+      {
+        "Father",
+        [
+          { "Nonna", [] },
+          { "Pops", [] }
+        ]
+      }
+    ]
+  },
+  ExpectedOutput = "- Me
+  - Mother
+    - Grandmother
+    - Grandfather
+  - Father
+    - Nonna
+    - Pops
+",
+  ?assert(format(FamilyTree) == ExpectedOutput).
--- string_tree_test.erl
+++ string_tree_test.erl
@@ -1,48 +1,6 @@
 #!/usr/bin/env escript
 
 main(_) ->
-  io:format("== Single Leaf Test~n"),
-  single_leaf_test(),
-  io:format("== Parent Child Test~n"),
-  parent_child_test(),
-  io:format("== Sibling Test~n"),
-  siblings_test(),
-  io:format("== Family Tree Test~n"),
-  family_tree_test().
+  compile:file(string_tree, [report_errors]),
+  string_tree:test().
 
-single_leaf_test() ->
-  Foo = {"Foo", []},
-  string_tree:format(Foo).
-
-parent_child_test() ->
-  Bar = {"Bar", []},
-  Foo = {"Foo", [Bar]},
-  string_tree:format(Foo).
-
-siblings_test() ->
-  Bar = {"Bar", []},
-  Baz = {"Baz", []},
-  Foo = {"Foo", [Bar, Baz]},
-  string_tree:format(Foo).
-
-family_tree_test() ->
-  FamilyTree = {
-    "Me",
-    [
-      {
-        "Mother",
-        [
-          { "Grandmother", [] },
-          { "Grandfather", [] }
-        ]
-      },
-      {
-        "Father",
-        [
-          { "Nonna", [] },
-          { "Pops", [] }
-        ]
-      }
-    ]
-  },
-  string_tree:format(FamilyTree).
