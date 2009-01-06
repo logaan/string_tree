ee1529a96a9a90df6d2e46c4e89c6f4f337f3939
Colin Campbell-McPherson <colin@logaan.net>
2009-01-06 17:40:29 +0900

    cleaned up comments

--- string_tree.erl
+++ string_tree.erl
@@ -3,28 +3,26 @@
 -include_lib("eunit/include/eunit.hrl").
 
 % TODO:
-% - If we made leafs to be just strings then the same method that prints the
-%   string in a non-leaf node could be used to handle leaf nodes.
 % - Output shouldn't be passed to every level, the lowest levels should just
 %   return how they're to be represented and higher levels should join them.
-% - There might be a way to cut down on code by directly working on the contents
-%   of the head of a list like [{String, Children} | Tail]
 
 format(StringTree) -> format(StringTree, 0, "").
 
-% Leaves are in the format {String, []}
 % Regular nodes are in the format {String, [Node1, Leaf1, ... NodeN, LeafN]}
 format({String, Children}, Indentation, Output) ->
   NewOutput = format(String, Indentation, Output),
   format(Children, Indentation + 1, NewOutput);
 
+% Empty list of children
 format([], Indentation, Output) ->
   Output;
 
-format([H|T], Indentation, Output) when is_list(H); is_tuple(H) ->
-  HeadOutput = format(H, Indentation, Output),
-  format(T, Indentation, HeadOutput);
+% List of children
+format([Head|Tail], Indentation, Output) when is_list(Head); is_tuple(Head) ->
+  HeadOutput = format(Head, Indentation, Output),
+  format(Tail, Indentation, HeadOutput);
 
+% Leaf node or branch node text
 format(String, Indentation, Output) ->
   IndentationString = string:chars($ , Indentation * 2),
   string:join([Output, IndentationString, "- ", String, "\n"], "").
